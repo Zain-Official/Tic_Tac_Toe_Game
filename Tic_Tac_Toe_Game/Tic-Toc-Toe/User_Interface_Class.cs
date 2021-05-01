4111b60c18de96e06d987e56bed7bf95e2ed696b
Zain <zainofficial03@gmail.com>
2021-05-01 17:01:43 +0500

    Add project files.

---
+++ Tic_Tac_Toe_Game/Tic-Toc-Toe/User_Interface_Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe_Game.Tic_Toc_Toe
+{
+    class User_Interface_Class
+    {
+        public void Display_Design()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
